0049f493584a07edf10ebe97ccc28c144647c712
wandersondv <wanderson.s.s1@gmail.com>
2023-08-25 16:54:50 -0300

    fix_1: corrige pages pasta Pages para page e adiciona icons em paginas App_MP, Projects,Contact

--- App_MP.py
+++ App_MP.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title='multipage App',
-    page_icon='😤'
+    page_icon='☕'
 )
 st.title('Main Page')
 st.sidebar.success('Select a page above')
