0e41c19b2fcefe00b011d6b5636e22577003fa0b
wandersondv <wanderson.s.s1@gmail.com>
2023-08-25 17:16:00 -0300

    feat_4: adiciona textbox e submit     Breaking change: App_MP.py button and textbox

--- 1_🪙_App_MP.py
+++ 1_🪙_App_MP.py
@@ -6,3 +6,11 @@ st.set_page_config(
 )
 st.title('Main Page')
 st.sidebar.success('Select a page above')
+
+if 'my_input' not in st.session_state:
+    st.session_state['my_input'] = ''
+my_input = st.text_input('Input a text here', st.session_state['my_input'])
+submit = st.button('Submit')
+if submit:
+    st.session_state['my_input']= my_input
+    st.write('You have entered', my_input)
